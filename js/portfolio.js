b5951d9548dc692bfaf235f880b0c23f2047b224
kevintran01 <Kevintran04024@gmail.com>
2018-11-01 15:22:15 -0500

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
         $('#hide').click(function() {
-            $('#project1').hide();
+            $('#project1').show();
         });
         $('#show').click(function() {
-            $('#project1').show();
+            $('#project1').hide();
         });
 });
