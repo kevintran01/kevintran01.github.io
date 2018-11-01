db253521e44d834174fddf63513aacdb479ffdab
kevintran01 <Kevintran04024@gmail.com>
2018-11-01 14:59:30 -0500

    update

---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+        $('#hide').click(function() {
+            $('#project1').hide();
+        });
+        $('#show').click(function() {
+            $('#project1').show();
+        });
+});
