c07c35504c9dacdc57df0b605bc17448996e01ae
kevintran01 <Kevintran04024@gmail.com>
2018-11-09 15:56:34 -0600

    update

--- Responsive-Web-Exercises/markdownpreviewer/src/App.js
+++ Responsive-Web-Exercises/markdownpreviewer/src/App.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import FormGroup from 'react-bootstrap/lib/FormGroup'
+import ControlLabel from 'react-bootstrap/lib/ControlLabel'
+import FormControl from 'react-bootstrap/lib/FormControl'
+
+let marked = require("marked");
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
 
-      </div>
-    );
-  }
+    state = {
+        markdown: ""
+    }
+
+    updateMarkdown = function (markdown) {
+        this.setState({markdown});
+    }
+
+    render() {
+        let {markdown} = this.state;
+        console.log(markdown);
+        return (
+            <div className="App container">
+                <div>
+                    <FormGroup controlId="formControlsTextarea">
+                        <ControlLabel>Markdown Input</ControlLabel>
+                        <FormControl componentClass="textarea" placeholder="Enter Markdown"
+                                     value={markdown} onChange={(event)=>this.updateMarkdown(event.target.value)}></FormControl>
+                    </FormGroup>
+                </div>
+                <div>
+                    <h1>Markdown Output</h1>
+                    <div dangerouslySetInnerHTML = {{__html: marked(markdown)}}>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
