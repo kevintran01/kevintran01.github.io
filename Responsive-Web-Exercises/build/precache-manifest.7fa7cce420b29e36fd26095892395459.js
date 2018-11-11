2955385d1cdebe6d1e31124b978e1cdef5e534fb
kevintran01 <Kevintran04024@gmail.com>
2018-11-11 01:08:58 -0600

    update

---
+++ Responsive-Web-Exercises/build/precache-manifest.7fa7cce420b29e36fd26095892395459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af98a661a57589aee0d2",
+    "url": "/static/js/main.af98a661.chunk.js"
+  },
+  {
+    "revision": "9f5526c976df47757b31",
+    "url": "/static/js/1.9f5526c9.chunk.js"
+  },
+  {
+    "revision": "af98a661a57589aee0d2",
+    "url": "/static/css/main.046268a2.chunk.css"
+  },
+  {
+    "revision": "aaa20e91ae0928bcead365c2430596ba",
+    "url": "/index.html"
+  }
+];
